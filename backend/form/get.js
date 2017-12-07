99a2b6ed1cfcf41536ea3b478618e262d17ea819
Phodal HUANG <h@phodal.com>
2017-12-07 11:07:14 +0800

    [GET] add basic

--- backend/form/get.js
+++ backend/form/get.js
@@ -1,7 +1,32 @@
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 module.exports.handler = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: {}
+  const params = {
+    TableName: process.env.FORM_DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.formId,
+    },
   };
-  callback(null, response);
+  console.log(params);
+
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'couldn\'t fetch the form item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
 };
